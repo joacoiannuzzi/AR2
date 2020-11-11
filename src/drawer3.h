400c6020f67795d2c791aa88ddc7eba835185f71
Joaquin Iannuzzi <joaco.iannuzzi@gmail.com>
2020-11-11 16:57:03 -0300

    added glm to cmake

--- src/drawer3.h
+++ src/drawer3.h
@@ -18,8 +18,8 @@
 #include <openvslam/type.h>
 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
 
 #include "shader.h"
 #include "stb_image.h"
